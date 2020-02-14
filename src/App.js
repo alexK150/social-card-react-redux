e808aae56ad5450fb542d2e3939ca3dc359f804b
Alex <akuzin20@gmail.com>
2020-02-14 10:14:20 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import backgroundIMG from './assets/orange-bg.jpg';
+
+
+const App = () => {
+    useEffect(() => {
+        // Initializes Materialize JS
+        M.AutoInit()
+    });
+
+    return <div className="App">
+        <div className="row">
+            <div className="col s12 m7">
+                <div className="card">
+                    <div className="card-image">
+                        <img src={backgroundIMG}/>
+                        <span className="card-title">Card Title</span>
+                    </div>
+                    <div className="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  );
-}
+};
 
 export default App;
